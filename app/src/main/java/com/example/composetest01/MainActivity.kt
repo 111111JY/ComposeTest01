df872ebd2a70d4c5e9590b9630080efa2b79ebdc
JY <weiweijun96@gmail.com>
2021-05-17 18:03:30 +0800

    modify

--- app/src/main/java/com/example/composetest01/MainActivity.kt
+++ app/src/main/java/com/example/composetest01/MainActivity.kt
@@ -19,8 +19,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
-                    Greeting("rdgg")
-                }
+                                   }
             }
         }
     }
