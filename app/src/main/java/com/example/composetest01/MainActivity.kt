b12b78e3136084b65080e47ee37e8164abd2878a
JY <weiweijun96@gmail.com>
2021-05-18 12:13:27 +0800

    learn chapter02:add some UI

--- app/src/main/java/com/example/composetest01/MainActivity.kt
+++ app/src/main/java/com/example/composetest01/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.composetest01
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -15,13 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetest01.ui.theme.ComposeTest01Theme
 
 class MainActivity : ComponentActivity() {
-    var inputText = mutableListOf("")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -35,9 +31,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTest01Theme {
+        NewsStory(name = "JimYoung")
+    }
+}
+
 @Composable
 fun NewsStory(name: String) {
-    var modifyName by mutableStateOf(name)
+    val modifyName = remember {
+        mutableStateOf(name)
+    }
+
+    var selected by remember {
+        mutableStateOf(true)
+    }
+
+    var showed by remember {
+        mutableStateOf(true)
+    }
+
     Column(modifier = Modifier.padding(20.dp)) {
         Image(
             painter = painterResource(id = R.drawable.header),
@@ -45,27 +61,36 @@ fun NewsStory(name: String) {
             modifier = Modifier
                 .height(180.dp)
                 .fillMaxWidth()
-                .clip(shape = RoundedCornerShape(10.dp)),
+                .clip(
+                    shape = RoundedCornerShape(
+                        if (selected) 15.dp else 0.dp
+                    )
+                ),
             contentScale = ContentScale.Crop
         )
 
         Spacer(Modifier.height(16.dp))
 
         Text(
-            text = "Hi,$modifyName!",
+            text = "Hi,${modifyName.value}!",
             style = typography.h5,
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis
+            maxLines = 2
         )
+
         Text(text = "This is your first time to study jetpack compose.", style = typography.body1)
+
+        Spacer(Modifier.height(16.dp))
+
         Text(text = "May 2021", style = typography.body2)
 
         Spacer(Modifier.height(16.dp))
 
         OutlinedTextField(
-            value = modifyName,
-            onValueChange = {modifyName = it},
+            value = modifyName.value,
+            onValueChange = { modifyName.value = it },
             label = { Text(text = "Name") },
+            modifier = Modifier.fillMaxWidth(),
+            maxLines = 2,
         )
 
         Spacer(Modifier.height(16.dp))
@@ -74,17 +99,47 @@ fun NewsStory(name: String) {
             .clip(shape = RoundedCornerShape(5.dp))
             .fillMaxWidth(),
             onClick = {
-                modifyName = "周洁仪"
+                selected = !selected
             }) {
-            Text(text = "Change Name")
+            Text(text = "Change Image RoundCornerShape")
+        }
+
+        Spacer(Modifier.height(16.dp))
+
+        Column{
+            Button(modifier = Modifier
+                .clip(shape = RoundedCornerShape(5.dp))
+                .fillMaxWidth(),
+                onClick = {
+                    showed = !showed
+                }) {
+                Text(text = "Show Second Image")
+            }
+
+            Spacer(Modifier.height(16.dp))
+
+            ShowSecondImage(show = showed)
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeTest01Theme {
-        NewsStory(name = "JimYoung")
+fun ShowSecondImage(show: Boolean) {
+    if (show) {
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(
+                        shape = RoundedCornerShape(
+                            15.dp
+                        )
+                    ),
+                contentScale = ContentScale.Crop
+            )
+        }
     }
 }
